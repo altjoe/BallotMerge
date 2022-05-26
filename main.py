4cd31d97875fe4cbf8268859cb58c9c2ca83457e
altjoe <meyer.joer5@gmail.com>
2022-05-26 09:50:57 -0400

    working

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from PyPDF2 import PdfFileMerger, PdfFileReader
+from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 import glob
+from pdfminer.high_level import extract_text
+import os 
 
 def main():
-    merger = PdfFileMerger()
-    url = "SeparateBallots/"
-    pdf_files = glob.glob(f'{url}*.pdf')
+  
     
+    pdf_files = glob.glob(f'SeparateBallots/*.pdf')
+    
+    count = 0
     for file in pdf_files:
-        merger.append(PdfFileReader(open(file, 'rb')))
+        reader = PdfFileReader(open(file, 'rb'))
+        for page in reader.pages:
+            writer = PdfFileWriter()
+            writer.addPage(page)
+            with open(f'sep/output{count}.pdf', 'wb') as out:
+                writer.write(out)
+            count += 1
     
-    merger.write('Combined.pdf')
+    pdf_files = glob.glob(f'sep/*.pdf')
+
+    merge = PdfFileMerger()
+    for file in pdf_files:
+        reader = PdfFileReader(open(file, 'rb'))
+        with open(file, 'rb') as f:
+            text = extract_text(f)
+            if 'This page intentionally left blank' not in text:
+                merge.append(reader)
+        os.remove(f'{file}')
+    merge.write('Combined_Without_Blank.pdf')
 
 main()
